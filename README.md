b343350a237d0a8e4a915e1e836b55c03dead28d
David Thigpen <davidthigs@gmail.com>
2024-08-02 08:25:26 -0600

    Rewrite to use json transactions, improve report output

--- README.md
+++ README.md
@@ -1,11 +1,64 @@
 # Budget CLI
 
-A command line tool to manage financial expenses.
+A command line tool to manage generate personal finance expense reports.
 
-## Roadmap
+## Examples
 
-- Budget schema
-  - 
+```
+$ budget-cli budget.json ~/transactions/*.json
+Writing report: 2024-05-report.json
+Writing report: 2024-07-report.json
+```
+```json
+{
+  "lastUpdated": "2024-08-04T11:13:18",
+  "month": "2024-05",
+  "summary": {
+    "expense": {
+      "categories": {
+        "HOA": "105.00",
+        "Travel": "512.43",
+        "Internet": "60.43",
+        "Entertainment": "112.12",
+        "Groceries": "198.02",
+        "Water": "44.75"
+      }
+    },
+    "uncategorized": "36.07"
+  },
+  "transactions": {
+    "expense": {
+      "HOA": [
+        {
+          "date": "2024-05-01",
+          "description": "COMMUNITY HOA",
+          "amount": 105.0,
+          "account": "Some Credit Card"
+        }
+      ],
+      "Travel": [
+        {
+          "date": "2024-05-02",
+          "description": "SOUTHWEST AIRLINES",
+          "amount": 512.43,
+          "account": "Some Credit Card"
+        }
+      ],
+      "Internet": [
+        {
+          "date": "2024-05-08",
+          "description": "COMCASTCABLECOMM 800-COMCAST CO",
+          "amount": 60.43,
+          "account": "Some Credit Card"
+        }
+      ]
+    }
+  }
+}
+```
+
+## TODO
+
+- Show warning when multiple categories match same tranaction
 - Handle various actions
   - split/replace transaction
-  - one-time changes. eg. unique action/match
--- src/budget_cli/budget_cli.py
+++ src/budget_cli/budget_cli.py
@@ -4,7 +4,7 @@ from datetime import datetime, date
 import json
 from pathlib import Path
 import re
-from typing import Iterable
+from typing import Iterable, List
 from dataclasses import dataclass
 
 @dataclass
@@ -23,76 +23,212 @@ def matches_criteria(text, criteria: str | list[str]) -> bool:
         raise ValueError(f"Unsupported rule value {type(criteria)}")
     return False
 
-def run(budget: dict, transactions: Iterable[Transaction], start=None, end=None):
-    envelope_assignments = {'Uncategorized': []}
-    for t in transactions:
-        # print(t)
-        row_str = ", ".join(t.row)
-        is_match = False
-        for category in budget['categories']:
-            includes = category.get("includes", [])
-            excludes = category.get("excludes", [])
-            # continue to next category if excluded here
-            if any((matches_criteria(row_str, exc) for exc in excludes)):
-                continue
-            if any((matches_criteria(row_str, inc) for inc in includes)):
+def __transaction_matches_category(transaction: dict, category: dict) -> bool:
+    is_match = False
+    if category.get('includes'):
+        for inc in category.get('includes'):
+            is_inc_match = True
+            for col_name, col_pattern in inc.items():
+                col_str = str(transaction.get(col_name, ''))
+                if not re.search(col_pattern, col_str, re.I):
+                    is_inc_match = False
+                    break
+            if is_inc_match:
                 is_match = True
                 break
-        if is_match:
-            category_name = category.get('name')
-            assigned = envelope_assignments.get(category_name, [])
-            assigned.append(row_str)
-            envelope_assignments[category_name] = assigned
-        else:
-            envelope_assignments['Uncategorized'].append(row_str)
-    print(json.dumps(envelope_assignments, indent=2))
+    if is_match and category.get('excludes'):
+        for exc in category.get('excludes'):
+            for col_name, col_pattern in exc:
+                col_str = f'{transaction.get("col_name", "")}'
+                if re.match(col_pattern, col_str, re.I):
+                    is_match = False
+                             
+            if not is_match:
+                break
+    return is_match
 
-
-def existing_file(p: str) -> Path:
-    p = Path(p)
-    if p.is_file():
-        return p
-    raise argparse.ArgumentError(f"Path {p} must be an existing file")
-
-
-def parse_date_str(date_str: str) -> date:
-    if date_str == "now":
-        return datetime.today().date()
-    return date.fromisoformat(date_str)
+def group_by(items: list, key_getter) -> dict:
+    groups = {}
+    for item in items:
+        key = key_getter(item)
+        if key not in groups:
+            groups[key] = []
+        groups[key].append(item)
+    return groups
     
-def parse_datetime_str(datetime: str) -> datetime:
-    return date.fromisoformat(datetime)
-    
+def run(budget: dict, transactions: Iterable[Transaction], output_dir: Path):
+    print('Generating reports')
+    transactions = list(transactions)
+    actions = budget.get('actions', [])
+    # split/replace should be done before categorization because it adds/removes transactions
+    pre_categorization_actions = list(filter(lambda a: a.get('type') in ('split', 'replace'), actions))
+
+    post_categorization_actions = list(filter(lambda a: a.get('type') in ('hide',), actions))
+    unhandled_actions = list(filter(lambda a: a not in pre_categorization_actions and a not in post_categorization_actions, actions))
+    if unhandled_actions:
+        types = ', '.join(map(lambda x: x.get('type'), unhandled_actions))
+        raise ValueError(f'Actions with unknown types: {types}')
+
+    # handle pre_categorization_actions
+    removed_transactions = []
+    added_transactions = []
+    for action in pre_categorization_actions:
+
+        action_type = action['type']
+        if action_type in ('split', 'replace'):
+            incs = action.get('includes', [])
+            excs = action.get('excludes', [])
+            dummy_category = {'name': 'dummy', 'includes': incs, 'excludes': excs}
+            matching_transactions = list(filter(lambda x: transaction_matches_category(x, dummy_category)))
+            # e.g. replace/with split/into
+            replacements = action.get('into', action.get('with', []))
+            if not replacements:
+                raise ValueError('Replace/split actions must have an "into" or "with" property with replacement transaction details')
+                
+            removed_transactions.extend(matching_transactions)
+            for t in matching_transactions:
+                replacement_transactions = []
+                # TODO copy t
+                orig_amount = t['amount']
+                orig_amount_sign = 1 if t['amount'] >= 0 else -1
+                orig_amount_abs = abs(orig_amount)
+                for repl in replacements:
+                    repl_amount = repl.pop('amount', None)
+                    repl_transaction = {**json.loads(json.dumps(repl)), **json.loads(json.dumps(repl))}
+                    new_amount = None
+                    if isinstance(repl_amount, (int, float)):
+                        # TODO handle sign
+                        new_amount = repl_amount
+                    elif isinstance(repl_ammount, str):
+                        # TODO implement
+                        raise NotImplmentedError(f'Using percentages in replacements has not been implemented yet')
+                    else:
+                        raise ValueError(f'Invalid replacement amount: {repl_amount}')
+                    repl_transaction['amount'] = new_amount
+                    if repl_amount is not None:
+                        repl_transaction['note'] = f'Generated: Split {replacement_amount} of {orig_amount}'
+                    replacement_transactions.append(repl_transaction)
+                replacement_transactions_without_amounts = filter(lambda x:'amount' not in x, replacement_replacement_transactions)
+                if replacement_transactions:
+                    # TODO assign lefover amount evenly
+                    raise NotImplementedError(f'Replacements must include exact amount')
+                # TODO verify that everything adds up, if not add transaction?
+    preprocessed_transactions = [*filter(lambda x: x not in removed_transactions, transactions), *added_transactions]
+    preprocessed_transactions = sorted(preprocessed_transactions, key=lambda x: x['date'])
+
+    # categorize transactions
+    for transaction in filter(lambda x: not x.get('category'), preprocessed_transactions):
+        found_category = False
+        for category in budget.get('categories',[]):
+            if __transaction_matches_category(transaction, category):
+                found_category = True
+                transaction['category'] = category['name']
+                break
+        if not found_category:
+            transaction['category'] = 'uncategorized'
+            # exit()
+    hidden_transactions = []
+    for action in post_categorization_actions:
+        action_type = action['type']
+        if action_type == 'hide':
+            categories_to_hide = action.get('categories', [])
+            incs = action.get('includes', [])
+            excs = action.get('excludes', [])
+            dummy_category = {'name': 'dummy', 'includes': incs, 'excludes': excs}
+            matching_transactions = []
+            matching_transactions.extend(filter(lambda x: x['category'] in categories_to_hide, transactions))
+            matching_transactions.extend(filter(lambda x: __transaction_matches_category(x, dummy_category), transactions))
+            hidden_transactions.extend(matching_transactions)
+
+    transactions_after_post_actions = list(filter(lambda x: x not in hidden_transactions, preprocessed_transactions))
+    report = {
+        'monthly': [
+            # month: 2024-04,
+            # categories: []
+        ]
+    }
+    month_groups = group_by(transactions_after_post_actions, lambda t: t['date'][:7])
+    group_entries = sorted(month_groups.items(), key=lambda x: x[0])
+    for year_month, month_transactions in group_entries:
+        month_report = {
+            'lastUpdated': datetime.now().isoformat()[:19],
+            'month': year_month,
+            'summary': {
+            },
+            'transactions': {
+                
+            }
+        }
+        report['monthly'].append(month_report)
+        category_groups = group_by(month_transactions, lambda t: t['category'])
+        for category_name, category_transactions in category_groups.items():
+            category_total = sum(map(lambda t: t['amount'], category_transactions))
+            if category_name == 'uncategorized':
+                category_type = 'uncategorized'
+            else:
+                category_type = next((c for c in budget.get('categories', []) if c['name'] == category_name), {'type': 'expense'})['type']
+            if category_type == 'uncategorized':
+                month_report['summary'][category_type] = '{:.2f}'.format(category_total)
+            else:
+                if category_type not in month_report['summary']:
+                    month_report['summary'][category_type] = { 'categories': {}}
+                month_report['summary'][category_type]['categories'][category_name] = '{:.2f}'.format(category_total)
+            # TODO remove category from transactions
+            def without_categories(t):
+                t = json.loads(json.dumps(t)) # copy dict
+                t.pop('category')
+                return t
+            if category_type == 'uncategorized':
+                month_report['transactions'][category_type] = list(map(without_categories, category_transactions))
+            else:
+                if category_type not in month_report['transactions']:
+                    month_report['transactions'][category_type] = {}
+                
+                month_report['transactions'][category_type][category_name] = list(map(without_categories, category_transactions))
+        # reinsert uncategorized so it goes after the other
+        if 'uncategorized' in month_report['summary']:
+            month_report['summary']['uncategorized'] = month_report['summary'].pop('uncategorized')
+        if 'uncategorized' in month_report['transactions']:
+            month_report['transactions']['uncategorized'] = month_report['transactions'].pop('uncategorized')
+        month_report_file = output_dir / f'{year_month}-report.json'
+        print(f'Writing report: {month_report_file}')
+        month_report_file.write_text(json.dumps(month_report, indent=2))
+
+def read_transactions(paths: List[Path]):
+    for p in paths:
+        transactions = json.loads(p.read_text())
+        for t in transactions:
+            yield t
+
+def parse_budget(budget_path: Path) -> dict:
+    DEFAULT_BUDGET = {
+        'categories': [],
+        'actions': [],
+    }
+    budget = json.loads(budget_path.read_text())
+    budget = {**DEFAULT_BUDGET, **budget}
+    return budget
+
 def _main():
+    def existing_file(p: str) -> Path:
+        p = Path(p)
+        if p.is_file():
+            return p
+        raise argparse.ArgumentError(f"Path {p} must be an existing file")
+
     parser = argparse.ArgumentParser(
-        description="A simple command line tool to manage expenses"
+        description="A command line tool to generate budgeting reports from transactions"
     )
-    parser.add_argument("budget", type=existing_file, help="Path to budget json file")
+    parser.add_argument("budget_file", type=existing_file, help="Path to budget json file")
     parser.add_argument(
-        "transactions", nargs='+', type=existing_file, help="Path to transactions csv file"
+        "transactions_files", nargs='+', type=existing_file, help="Path to transactions json files"
     )
-    parser.add_argument("--date-col", type=int, default=0, help="column index of the date of the transaction")
-    parser.add_argument("--amount-col", type=int, default=1, help="column index of the amount of the transaction")
-    parser.add_argument("--name-col", type=str, default=2, help="column index of the name of the transaction")
-    parser.add_argument("--start", type=parse_date_str, help="start date of transaction history in YYYY-MM-DD format")
-    parser.add_argument("--end", type=parse_date_str, help="end date of transaction history in YYYY-MM-DD format")
-
+    parser.add_argument('-o','--output-dir', type=Path, default=Path.cwd(), help='Directory to output reports to')
     args = parser.parse_args()
-    budget = json.loads(args.budget.read_text())
-    # read in and sort all transactions
-    all_transaction_rows = []
-    for t in args.transactions:
-        with open(t, newline="") as csvfile:
-            transactions_reader = csv.reader(csvfile)
-            next(transactions_reader)
-            file_transactions = list(transactions_reader)
-            if args.start and args.end:
-                file_transactions = filter(lambda r: args.start <= parse_datetime_str(r[args.date_col]) <= args.end, file_transactions)
-            all_transaction_rows.extend(file_transactions)
-    all_transaction_rows.sort(key=lambda r: parse_datetime_str(r[args.date_col]))
-    all_transaction = map(lambda r: Transaction(parse_datetime_str(r[args.date_col]), float(r[args.amount_col]), r[args.name_col], row=r), all_transaction_rows)
-    run(budget, all_transaction)
-
-
+    budget = parse_budget(args.budget_file)
+    transactions= read_transactions(args.transactions_files)
+    run(budget, transactions, args.output_dir)
+    
+    
 if __name__ == "__main__":
     _main()
